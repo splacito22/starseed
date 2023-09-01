b8f7a139905cffcf04c6a073e74c7192281e5b81
Stephany <136421961+splacito22@users.noreply.github.com>
2023-08-31 20:45:39 -0700

    did readme file dynamically

---
+++ README.md
@@ -0,0 +1,31 @@
+# starseed
+  ## Description
+    readme
+
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Installation
+    node index, index.js, inquirer
+
+    ## Usage
+    screenshot
+
+    ## License
+    This project is licensed under the MIT license.
+
+    ## Contributing
+    none
+
+    ## Tests
+    testing here
+
+    ## Questions
+    For questions about this project, please contact splacito22@gmail.com.
+    GitHub: [splacito22](https://github.com/splacito22)
+  
--- index.js
+++ index.js
@@ -1,13 +1,115 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "Enter the project title:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of the project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "Apache", "GPL", "None"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter test instructions:",
+  },
+  {
+    type: "input",
+    name: "githubUsername",
+    message: "Enter your GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function generateReadmeContent(answers) {
+  // Use the answers to generate your README content
+  return `# ${answers.projectTitle}
+  ## Description
+    ${answers.description}
+
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Installation
+    ${answers.installation}
+
+    ## Usage
+    ${answers.usage}
+
+    ## License
+    This project is licensed under the ${answers.license} license.
+
+    ## Contributing
+    ${answers.contributing}
+
+    ## Tests
+    ${answers.tests}
+
+    ## Questions
+    For questions about this project, please contact ${answers.email}.
+    GitHub: [${answers.githubUsername}](https://github.com/${answers.githubUsername})
+  `;
+}
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error("Error writing README file:", err);
+    } else {
+      console.log("README file generated successfully!");
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // Generate README content based on answers
+      const readmeContent = generateReadmeContent(answers);
+
+      // Write README file
+      writeToFile("README.md", readmeContent);
+    })
+    .catch((error) => {
+      console.error("Error initializing app:", error);
+    });
+}
 
 // Function call to initialize app
 init();
